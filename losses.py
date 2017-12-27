b1fbe91ff13f20557ce32e6682fbc16c8427ab5f
Fitsum Reda <freda@dhcp-172-20-232-236.nvidia.com>
2017-12-27 15:34:08 -0800

    fix multi-gpu training error for multi-scale loss

--- losses.py
+++ losses.py
@@ -79,7 +79,7 @@ class MultiScale(nn.Module):
                 target_ = self.multiScales[i](target)
                 epevalue += self.loss_weights[i]*EPE(output_, target_)
                 lossvalue += self.loss_weights[i]*self.loss(output_, target_)
-            return ['MultiScale-'+self.l_type, 'EPE'], [lossvalue, epevalue]
+            return [lossvalue, epevalue]
         else:
             epevalue += EPE(output, target)
             lossvalue += self.loss(output, target)
