9ba2b0c640b4a01e00afbeb274bcfa1a36d4cd76
Fitsum Reda <fitsumreda@users.noreply.github.com>
2017-12-27 16:18:44 -0800

    Update download_caffe_models.sh

--- download_caffe_models.sh
+++ download_caffe_models.sh
@@ -2,11 +2,6 @@
 git clone https://github.com/lmb-freiburg/flownet2-docker
 cd flownet2-docker
 
-sed -i '/cd models && /d' Dockerfile
-sed -i '/bash download-models.sh && /d' Dockerfile
-sed -i '/rm flownet2-models.tar.gz && /d' Dockerfile
-sed -i '/cd .. && /d' Dockerfile
-
 sed -i '$ a RUN apt-get update && apt-get install -y python-pip \
 RUN pip install --upgrade pip \
 RUN pip install numpy -I \
