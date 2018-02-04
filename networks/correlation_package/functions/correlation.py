dafdc9b5cb8fa4c65285aad22b1429549d06d71a
Kai Chen <chenkaidev@gmail.com>
2018-02-04 16:29:32 +0800

    modify ops according to pytorch recommendations

--- networks/correlation_package/functions/correlation.py
+++ networks/correlation_package/functions/correlation.py
@@ -1,49 +1,55 @@
-import torch
-from torch.autograd import Function
+from torch.autograd import Function, Variable
 from .._ext import correlation
 
 
 class CorrelationFunction(Function):
 
-    def __init__(self, pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1):
-        super(CorrelationFunction, self).__init__()
-        self.pad_size = pad_size
-        self.kernel_size = kernel_size
-        self.max_displacement = max_displacement
-        self.stride1 = stride1
-        self.stride2 = stride2
-        self.corr_multiply = corr_multiply
-        # self.out_channel = ((max_displacement/stride2)*2 + 1) * ((max_displacement/stride2)*2 + 1)
+    @staticmethod
+    def forward(ctx,
+                input1,
+                input2,
+                pad_size=3,
+                kernel_size=3,
+                max_displacement=20,
+                stride1=1,
+                stride2=2,
+                corr_multiply=1):
+        assert input1.is_contiguous()
+        assert input2.is_contiguous()
 
-    def forward(self, input1, input2):
-        self.save_for_backward(input1, input2)
+        ctx.save_for_backward(input1, input2)
+        ctx.pad_size = pad_size
+        ctx.kernel_size = kernel_size
+        ctx.max_displacement = max_displacement
+        ctx.stride1 = stride1
+        ctx.stride2 = stride2
+        ctx.corr_multiply = corr_multiply
 
-        assert(input1.is_contiguous() == True)
-        assert(input2.is_contiguous() == True)
+        rbot1 = input1.new()
+        rbot2 = input2.new()
+        output = input1.new()
 
-        with torch.cuda.device_of(input1):
-            rbot1 = input1.new()
-            rbot2 = input2.new()
-            output = input1.new()
-
-            correlation.Correlation_forward_cuda(input1, input2, rbot1, rbot2, output, 
-                self.pad_size, self.kernel_size, self.max_displacement,self.stride1, self.stride2, self.corr_multiply)
+        correlation.Correlation_forward_cuda(
+            input1, input2, rbot1, rbot2, output, pad_size, kernel_size,
+            max_displacement, stride1, stride2, corr_multiply)
 
         return output
 
-    def backward(self, grad_output):
-        input1, input2 = self.saved_tensors
-        
-        assert(grad_output.is_contiguous() == True)
+    @staticmethod
+    def backward(ctx, grad_output):
+        assert grad_output.is_contiguous()
 
-        with torch.cuda.device_of(input1):
-            rbot1 = input1.new()
-            rbot2 = input2.new()
+        input1, input2 = ctx.saved_tensors
 
-            grad_input1 = input1.new()
-            grad_input2 = input2.new()
+        rbot1 = input1.new()
+        rbot2 = input2.new()
 
-            correlation.Correlation_backward_cuda(input1, input2, rbot1, rbot2, grad_output, grad_input1, grad_input2,
-                self.pad_size, self.kernel_size, self.max_displacement,self.stride1, self.stride2, self.corr_multiply)
+        grad_input1 = Variable(input1.new())
+        grad_input2 = Variable(input2.new())
 
-        return grad_input1, grad_input2
+        correlation.Correlation_backward_cuda(
+            input1, input2, rbot1, rbot2, grad_output.data, grad_input1.data,
+            grad_input2.data, ctx.pad_size, ctx.kernel_size,
+            ctx.max_displacement, ctx.stride1, ctx.stride2, ctx.corr_multiply)
+
+        return (grad_input1, grad_input2) + (None, ) * 6
