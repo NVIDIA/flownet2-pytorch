1a4880d9458dd89078c1fff757254c2ecf7d7cad
JackyLiu <jjkka132@gmail.com>
2018-04-07 06:41:23 +0000

    make.sh for python3

--- networks/correlation_package/make.sh
+++ networks/correlation_package/make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-TORCH=$(python -c "import os; import torch; print(os.path.dirname(torch.__file__))")
+TORCH=$(python3 -c "import os; import torch; print(os.path.dirname(torch.__file__))")
 
 cd src
 
@@ -11,4 +11,4 @@ rm -r ../_ext
 nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 
 cd ../
-python build.py
+python3 build.py
