239ac299b1054d99820ea82a50e4c1dc187583f3
Mark Gituma <gitumarkk@gmail.com>
2019-06-25 11:01:47 -0400

    updated launch script

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo nvidia-docker build -t $USER/pytorch:CUDA8-py27 .
-sudo nvidia-docker run --rm -ti --volume=$(pwd):/flownet2-pytorch:rw --workdir=/flownet2-pytorch --ipc=host $USER/pytorch:CUDA8-py27 /bin/bash
+sudo nvidia-docker build -t flownet2-pytorch:CUDA9-py35 .
+sudo nvidia-docker run --rm -ti --volume=$(pwd):/flownet2-pytorch:rw --workdir=/flownet2-pytorch --ipc=host flownet2-pytorch:CUDA9-py35 /bin/bash
