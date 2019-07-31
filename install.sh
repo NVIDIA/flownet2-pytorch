39c8153f321f0c8a7fbc2026466eef6e6e7a4dfd
Fitsum Reda <freda@nvidia.com>
2019-07-31 13:57:44 -0700

    remove build files before re-installation

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cd ./networks/correlation_package
+rm -rf *_cuda.egg-info build dist __pycache__
 python3 setup.py install --user
-cd ../resample2d_package 
+
+cd ../resample2d_package
+rm -rf *_cuda.egg-info build dist __pycache__
 python3 setup.py install --user
-cd ../channelnorm_package 
+
+cd ../channelnorm_package
+rm -rf *_cuda.egg-info build dist __pycache__
 python3 setup.py install --user
+
 cd ..
